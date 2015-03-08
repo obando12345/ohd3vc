9164dc8d6b3f29a6f2790123f9409f34da6476ac
Takeshi Tezuka <tteduka@yahoo-corp.jp>
2015-03-08 11:08:50 +0900

    マージ

--- cgi-bin/login.php
+++ cgi-bin/login.php
@@ -55,10 +55,114 @@ if(!$session) {
 
 if(isset($response)) {
     $graphObject = $response->getGraphObject();
-    print_r($graphObject->getProperty('relationship_status'));
-    print_r($graphObject->getProperty('friends')->getProperty('summary')->getProperty('total_count'));
-}
+    
+    $rea10_count = 0;
 
+    // $married[] = '結婚しているか？';
+    $married[] = $graphObject->getProperty('relationship_status');
+    if($married[0] == 'Married') {
+        $married[] = '既婚';
+        $married[] = 'NG';
+        $rea10_count++;
+    } else {
+        $married[] = '未婚';
+        $married[] = 'OK';
+    }
+
+    // $friends[] = '友達の数';
+    $friends[] = $graphObject->getProperty('friends')->getProperty('summary')->getProperty('total_count');
+    if($friends[0] >= 100) {
+        $friends[] = '多い';
+        $friends[] = 'NG';
+        $rea10_count++;
+    } else {
+        $friends[] = '少ない';
+        $friends[] = 'OK';
+    }
+
+    $result = true;
+    if($rea10_count > 0) {
+        $result = false;
+    }
+
+$page = <<<EOD
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/my.css">
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $(function(){
+            var data = [
+                {question: "友達の数", reason: "$friends[0]人：$friends[1]", status: "$friends[2]"},
+                {question: "結婚しているか？", reason: "$married[1]", status: "$married[2]"},
+                {result: $result}
+            ];
+
+            $.each(data, function(i, val){
+                var ele_id = "ele_"+i;
+
+                var canvas = document.getElementById("clear_box");
+
+                if ( ! canvas || ! canvas.getContext ) {
+                    return false;
+                }
+                canvas.getContext('2d');
+                var ctx = canvas.getContext("2d");
+                ctx.beginPath();
+                ctx.clearRect(0, 0, 64, 64);
+
+                var result_img = canvas.toDataURL();
+
+                var $ele = $("<li style='font-size:x-large' class='media slideLeft'><div class='media-left'><img id='"+ele_id+"' class='media-object' src='"+result_img+"'></div><div class='media-body'><h3 class='media-heading'>"+val.question+"</h3>"+val.reason+"</div></li><hr>");
+
+                var delay = 3000*(i+1);             
+                setTimeout(function(){
+                    if('result' in val && val.result == false) {
+                        $.ajax({
+                            type: "GET",
+                            url: "sample/cloudbits.php?percent=100",
+                            success: function(){
+                                console.log("ajax success");
+                            },
+                            error: function(req, st, e) {
+                                consol.log(st);
+                            }
+                        });
+                        return;
+                    }
+                    $("#list").append($ele);
+                    $('html,body').animate({ scrollTop: $("#bottom").offset().top }, 'fast');
+                    setTimeout(function(){
+                        if(val.status == "OK") {
+                            var result_img = "check-mark-10-64.png";
+                        } else {
+                            var result_img = "x-mark-64.png";
+                        }
+                        $("#"+ele_id).attr("src", "img/"+result_img);
+                    }, 2000);
+                }, delay);
+            });
+        });
+    </script>
+</head>
+<body>
+    <div id="contents" class="container">
+        <h2 class="jumbotron">判定結果</h2>
+        <ul id="list" class="media-list">
+        </ul>
+    </div>
+    <div id="bottom"></div>
+    <canvas id="clear_box" style="display:none;" width="64" height="64"></canvas>
+</body>
+</html>
+
+
+EOD;
+} else {
 $page = <<<EOD
 <html>
 <head>
@@ -69,6 +173,7 @@ $page = <<<EOD
 </body>
 </html>
 EOD;
+}
 
 print $page;
 
